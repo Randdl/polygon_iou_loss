0b6803bee29bce3e97ef39ec377ec43f93273667
Randdl <alexlurand@gmail.com>
2022-04-18 11:04:46 -0500

    tuning params

--- train_detectron.py
+++ train_detectron.py
@@ -162,14 +162,14 @@ cfg = get_cfg()
 cfg.merge_from_file("configs/base_detection_faster_rcnn.yaml")
 cfg.DATASETS.TRAIN = ("Kitti_train",)
 cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 0
+# cfg.DATALOADER.NUM_WORKERS = 0
 # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 4
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+# cfg.SOLVER.IMS_PER_BATCH = 4
+# cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
 # cfg.SOLVER.MAX_ITER = 10    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
 # cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 16   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 16   # faster, and good enough for this toy dataset (default: 512)
+# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
 # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
